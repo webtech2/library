e06c3f530924a00f9da241437ec5438ca2865f3d
Darja <darja.solodovnikova@lu.lv>
2021-04-28 13:25:00 +0300

    register link added to login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,6 +6,12 @@
             </a>
         </x-slot>
 
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 underline">Register</a>
+                        @endif
+                </div>
+        
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
 
